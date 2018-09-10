1f596bb5a23362acfd47aec15feb2d0d4692ae82
Unknown <aaron.campbell115@gmail.com>
2018-09-10 10:10:57 -0600

    jump?

---
+++ 2D game/Assets/Scripts/:Users:56101:Desktop:github:Purgatory:2D game:Assets:Scripts:PC control.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCcontrol : MonoBehaviour {
+
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float Jumpheight;
+
+    // Player grounded variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround)
+	}
+
+	// Update is called once per frame
+	private void Update(){
+
+            //this code makes the character jump
+            if(Input.GetKeyDown(KeyCode.Space) && grounded){
+                Jump(); 
+            }
+        }
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Jumpheight)
+    }
+}
