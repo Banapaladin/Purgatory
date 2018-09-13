f562ed2db8607e28c8fdc3e49c12aba41190efce
Unknown <aaron.campbell115@gmail.com>
2018-09-13 11:59:50 -0600

    drop fail

---
+++ 2D game/Assets/Scripts/PCcontrolsolo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCcontrolsolo : MonoBehaviour {
+
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float Jumpheight;
+
+    // Player grounded variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	private void Update(){
+
+            //this code makes the character jump
+        if(Input.GetKeyDown(KeyCode.W) && grounded){
+                Jump(); 
+            }
+            //this is the drop command
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Drop();
+
+        // this is the A&D keys ror left and right.
+        if(Input.GetKey (KeyCode.D)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+        if(Input.GetKey (KeyCode.A)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+       
+    }
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Jumpheight);
+    }
+    public void Drop(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -Jumpheight);
+    }
+}
