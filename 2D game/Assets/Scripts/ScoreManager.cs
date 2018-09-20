d004913ab7de336b38504dd860e12b3231767c1a
Unknown <aaron.campbell115@gmail.com>
2018-09-20 10:27:48 -0600

    scores

---
+++ 2D game/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int Score;
+
+    Texture ScoreText;
+
+	private void Start(){
+        ScoreText = GetComponent<Text>();
+
+        Score = 0;
+	}
+
+	private void Update(){
+        if (Score < 0)
+            Score = 0;
+
+        ScoreText.text = " " + Score;
+	}
+
+    public static void AddPoints (int PointsToAdd) {
+        Score += PointsToAdd;
+    }
+
+    public static void Reset () {
+        Score = 0;
+    }
+
+
+}
