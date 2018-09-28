54bde6e42067bc10d5a6bebaee0d19156d03bd73
Unknown <aaron.campbell115@gmail.com>
2018-09-28 10:27:11 -0600

    respawn crap

---
+++ 2D game/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+        levelManager = FindObjectOfType<LevelManager>();	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision){
+        if(other.name == "PC"){
+            levelManager.CurrentCheckPoint = gameObject;
+            Debug.Log("Activated Checkpoint" + transform.position);
+        }
+	}
+}
---
+++ 2D game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject CurrentCheckPoint;
+    private Rigidbody2D PC;
+
+    //particles
+    public GameObject DeathParticle;
+    public GameObject RespawnParticle;
+
+    //respawn delay
+    public float RespawnDelay;
+
+    //point penalty
+    public int PointPenaltyOnDeath;
+
+    //gravitystorage
+    private float GravityStore;
+
+    // Use this for initialization
+	void Start () {
+        PC = FindObjectOfType<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RespawnPlayer(){
+        StartCoroutine("RespawnPCCo");
+    }
+
+    public IEnumerator RespawnPCCo(){
+        //generate death particles
+        Instantiate(DeathParticle, PC.transform.position, PC.transform.rotation);
+
+        //hide player
+        PC.enabled = false;
+        PC.GetComponent<Renderer>().enabled = false;
+
+        //gravity reset
+        GravityStore = PC.GetComponent<Rigidbody2D>().gravityScale;
+        PC.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        PC.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        //point penalty
+        ScoreManager.AddPoints(-PointPenaltyOnDeath);
+
+        //debug message
+        Debug,Log("PC Respawn");
+
+        //respawn delay
+        yield return new WaitForSeconds(RespawnDelay);
+
+        //gravity restore
+        PC.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+
+        //match pc's transform position
+        PC.transform.position = CurrentCheckPoint.transform.position;
+
+        //show player
+
+    }
+}
