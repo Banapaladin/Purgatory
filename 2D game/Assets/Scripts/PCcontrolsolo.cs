3133662faa4f69008f36161cb79c79b4c5e3dbc8
Unknown <aaron.campbell115@gmail.com>
2018-10-12 10:29:27 -0600

    oof

--- 2D game/Assets/Scripts/PCcontrolsolo.cs
+++ 2D game/Assets/Scripts/PCcontrolsolo.cs
@@ -66,6 +66,13 @@ public class PCcontrolsolo : MonoBehaviour {
         //comment this out when you turn off no-slip
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
+        //player flip
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            transform.localScale = new Vector3(-5f, 5f, 1f);
+
+        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+            transform.localScale = new Vector3(-5f, 5f, 1f);
+
     }
 
     public void Jump(){
--- 2D game/Assets/Scripts/Projectile.cs
+++ 2D game/Assets/Scripts/Projectile.cs
@@ -27,13 +27,15 @@ public class Projectile : MonoBehaviour {
         GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 
-	private void OnTriggerEnter(Collider2D other){
+	void OnTriggerEnter(Collider2D other){
         if(other.tag == "Enemy"){
             Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
             ScoreManager.AddPoints(PointsForKill);
         }
-
+        Instantiate(ProjectileParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
-	Instantiate(ProjectileParticle, transform.position, transform.rotation);
+
+
 }
