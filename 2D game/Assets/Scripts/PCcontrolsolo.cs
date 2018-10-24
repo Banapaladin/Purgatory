82e65a7f8b0c58b50227bce789ed3bd352a168b9
Unknown <aaron.campbell115@gmail.com>
2018-10-24 10:29:52 -0600

    yay

--- 2D game/Assets/Scripts/PCcontrolsolo.cs
+++ 2D game/Assets/Scripts/PCcontrolsolo.cs
@@ -68,7 +68,7 @@ public class PCcontrolsolo : MonoBehaviour {
 
         //player flip
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
-            transform.localScale = new Vector3(-5f, 5f, 1f);
+            transform.localScale = new Vector3(5f, 5f, 1f);
 
         else if (GetComponent<Rigidbody2D>().velocity.x < 0)
             transform.localScale = new Vector3(-5f, 5f, 1f);
