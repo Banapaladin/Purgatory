73fb6c5189ba6efa345d645579b60b95e5e6cade
Unknown <aaron.campbell115@gmail.com>
2018-11-28 10:30:10 -0700

    main menu

---
+++ 2D game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public int LevelToLoad;
+	
+	public void LoadLevel() {
+        SceneManager.LoadScene(LevelToLoad);
+	}
+	
+	// Update is called once per frame
+	public void  LevelExit() {
+        Application.Quit();
+	}
+}
